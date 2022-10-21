lightning-signer/validating-lightning-signer

persist: separate out seed persistence

lightning-signer-core/src/persist/mod.rs
@@ -56,7 +56,7 @@ pub trait Persist: Sync + Send {
     }
 
     /// Create a new node
-    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState, seed: &[u8]);
+    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState);
     /// Update node enforcement state
     fn update_node(&self, node_id: &PublicKey, state: &NodeState) -> Result<(), ()>;
     /// Delete a node and all of its channels.  Used in test mode.
@@ -104,7 +104,7 @@ pub struct DummyPersister;
 
 #[allow(unused_variables)]
 impl Persist for DummyPersister {
-    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState, seed: &[u8]) {}
+    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState) {}
 
     fn update_node(&self, node_id: &PublicKey, state: &NodeState) -> Result<(), ()> {
         Ok(())
@@ -159,4 +159,45 @@ impl Persist for DummyPersister {
     }
 
     fn clear_database(&self) {}
+}
+
+/// Seed persister
+pub trait SeedPersist: Sync + Send {
+    /// Persist the seed
+    fn put(&self, node_id: &PublicKey, seed: &[u8]);
+    /// Get the seed, if exists
+    fn get(&self, node_id: &PublicKey) -> Option<Vec<u8>>;
+}
+
+/// A null seed persister for testing
+pub struct DummySeedPersister;
+
+impl SeedPersist for DummySeedPersister {
+    fn put(&self, _node_id: &PublicKey, _seed: &[u8]) {}
+
+    fn get(&self, _node_id: &PublicKey) -> Option<Vec<u8>> {
+        None
+    }
+}
+
+/// A single in-memory seed persister - for testing
+pub struct MemorySeedPersister {
+    seed: Vec<u8>,
+}
+
+impl MemorySeedPersister {
+    /// Create a new in-memory seed persister
+    pub fn new(seed: Vec<u8>) -> Self {
+        Self { seed }
+    }
+}
+
+impl SeedPersist for MemorySeedPersister {
+    fn put(&self, _node_id: &PublicKey, _seed: &[u8]) {
+        unimplemented!()
+    }
+
+    fn get(&self, _node_id: &PublicKey) -> Option<Vec<u8>> {
+        Some(self.seed.clone())
+    }
 }

22388o/Validating-lightning-signer2

lightning-signer-core/src/persist/mod.rs
@@ -56,7 +56,7 @@ pub trait Persist: Sync + Send {
     }
 
     /// Create a new node
-    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState, seed: &[u8]);
+    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState);
     /// Update node enforcement state
     fn update_node(&self, node_id: &PublicKey, state: &NodeState) -> Result<(), ()>;
     /// Delete a node and all of its channels.  Used in test mode.
@@ -104,7 +104,7 @@ pub struct DummyPersister;
 
 #[allow(unused_variables)]
 impl Persist for DummyPersister {
-    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState, seed: &[u8]) {}
+    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &NodeState) {}
 
     fn update_node(&self, node_id: &PublicKey, state: &NodeState) -> Result<(), ()> {
         Ok(())
@@ -159,4 +159,45 @@ impl Persist for DummyPersister {
     }
 
     fn clear_database(&self) {}
+}
+
+/// Seed persister
+pub trait SeedPersist: Sync + Send {
+    /// Persist the seed
+    fn put(&self, key: &str, seed: &[u8]);
+    /// Get the seed, if exists
+    fn get(&self, key: &str) -> Option<Vec<u8>>;
+}
+
+/// A null seed persister for testing
+pub struct DummySeedPersister;
+
+impl SeedPersist for DummySeedPersister {
+    fn put(&self, _key: &str, _seed: &[u8]) {}
+
+    fn get(&self, _key: &str) -> Option<Vec<u8>> {
+        None
+    }
+}
+
+/// A single in-memory seed persister - for testing
+pub struct MemorySeedPersister {
+    seed: Vec<u8>,
+}
+
+impl MemorySeedPersister {
+    /// Create a new in-memory seed persister
+    pub fn new(seed: Vec<u8>) -> Self {
+        Self { seed }
+    }
+}
+
+impl SeedPersist for MemorySeedPersister {
+    fn put(&self, _key: &str, _seed: &[u8]) {
+        unimplemented!()
+    }
+
+    fn get(&self, _key: &str) -> Option<Vec<u8>> {
+        Some(self.seed.clone())
+    }
 }