lightning-signer/validating-lightning-signer

proxy: DRY integration test seed handling

vls-proxy/src/grpc/signer.rs
@@ -1,14 +1,12 @@
 use super::hsmd::{self, PingRequest, SignerRequest, SignerResponse};
-use crate::util::{
-    make_validator_factory, read_allowlist, read_integration_test_seed, write_integration_test_seed,
-};
+use crate::util::integration_test_seed;
+use crate::util::{make_validator_factory, read_allowlist};
 use http::Uri;
 use lightning_signer::bitcoin::Network;
 use lightning_signer::node::NodeServices;
 use lightning_signer::persist::Persist;
 use lightning_signer::signer::ClockStartingTimeFactory;
 use lightning_signer::util::clock::StandardClock;
-use lightning_signer::util::crypto_utils::maybe_generate_seed;
 use lightning_signer::util::status::Status;
 use lightning_signer_server::persist::kv_json::KVJsonPersister;
 use log::{error, info};
@@ -56,8 +54,7 @@ async fn connect(datadir: &str, uri: Uri, network: Network) {
     info!("ping result {}", reply.message);
     let (sender, receiver) = mpsc::channel(1);
     let response_stream = ReceiverStream::new(receiver);
-    let test_seed = read_integration_test_seed();
-    let seed = maybe_generate_seed(test_seed);
+    let seed = integration_test_seed();
     let persister: Arc<dyn Persist> = Arc::new(KVJsonPersister::new(&data_path));
     let allowlist = read_allowlist();
     let starting_time_factory = ClockStartingTimeFactory::new();
@@ -66,10 +63,6 @@ async fn connect(datadir: &str, uri: Uri, network: Network) {
     let services = NodeServices { validator_factory, starting_time_factory, persister, clock };
     let handler_builder =
         RootHandlerBuilder::new(network, 0, services, seed).allowlist(allowlist.clone());
-    // if no seed was provided by the integration test framework, persist the seed that we generated
-    if test_seed.is_none() {
-        write_integration_test_seed(&seed);
-    }
     let (root_handler, _muts) = handler_builder.build();
 
     // NOTE - For this signer mode it is easier to use the ALLOWLIST file to maintain the

vls-proxy/src/util.rs
@@ -9,6 +9,7 @@ use std::{env, fs};
 use lightning_signer::bitcoin::Network;
 use lightning_signer::policy::filter::PolicyFilter;
 use lightning_signer::policy::simple_validator::{make_simple_policy, SimpleValidatorFactory};
+use lightning_signer::util::crypto_utils::generate_seed;
 use lightning_signer::Arc;
 use tokio::runtime::{self, Runtime};
 
@@ -32,8 +33,20 @@ pub fn read_integration_test_seed() -> Option<[u8; 32]> {
     }
 }
 
-pub fn write_integration_test_seed(seed: &[u8; 32]) {
+fn write_integration_test_seed(seed: &[u8; 32]) {
     fs::write("hsm_secret", seed).expect("trouble writing hsm_secret");
+}
+
+/// Read integration test seed, and generate/persist it if it's missing
+pub fn integration_test_seed() -> [u8; 32] {
+    match read_integration_test_seed() {
+        None => {
+            let seed = generate_seed();
+            write_integration_test_seed(&seed);
+            seed
+        }
+        Some(seed) => seed,
+    }
 }
 
 #[cfg(feature = "main")]

vls-proxy/src/vls_main.rs
@@ -18,7 +18,7 @@ use lightning_signer::node::NodeServices;
 use lightning_signer::persist::{Mutations, Persist};
 use lightning_signer::signer::ClockStartingTimeFactory;
 use lightning_signer::util::clock::StandardClock;
-use lightning_signer::util::crypto_utils::{hkdf_sha256, maybe_generate_seed};
+use lightning_signer::util::crypto_utils::hkdf_sha256;
 use lightning_signer::Arc;
 use lightning_signer_server::nodefront::SingleFront;
 use lightning_signer_server::persist::kv_json::KVJsonPersister;
@@ -37,8 +37,8 @@ use vls_protocol_signer::vls_protocol;
 
 mod test;
 use vls_proxy::util::{
-    add_hsmd_args, bitcoind_rpc_url, handle_hsmd_version, make_validator_factory,
-    read_integration_test_seed, setup_logging, vls_network, write_integration_test_seed,
+    add_hsmd_args, bitcoind_rpc_url, handle_hsmd_version, integration_test_seed,
+    make_validator_factory, setup_logging, vls_network,
 };
 use vls_proxy::*;
 
@@ -266,17 +266,11 @@ async fn start() {
     let starting_time_factory = ClockStartingTimeFactory::new();
     let validator_factory = make_validator_factory(network);
     let clock = Arc::new(StandardClock());
-    let test_seed = read_integration_test_seed();
-    let seed = maybe_generate_seed(test_seed);
+    let seed = integration_test_seed();
     let persister = make_persister();
     let services = NodeServices { validator_factory, starting_time_factory, persister, clock };
     let handler_builder =
         RootHandlerBuilder::new(network, client.id(), services, seed).allowlist(allowlist);
-
-    // if no seed was provided by the integration test framework, persist the one we generated
-    if test_seed.is_none() {
-        write_integration_test_seed(&seed);
-    }
 
     let looper = make_looper(&seed).await;
 

22388o/Validating-lightning-signer2

vls-proxy/src/grpc/signer.rs
@@ -1,14 +1,12 @@
 use super::hsmd::{self, PingRequest, SignerRequest, SignerResponse};
-use crate::util::{
-    make_validator_factory, read_allowlist, read_integration_test_seed, write_integration_test_seed,
-};
+use crate::util::integration_test_seed_or_generate;
+use crate::util::{make_validator_factory, read_allowlist};
 use http::Uri;
 use lightning_signer::bitcoin::Network;
 use lightning_signer::node::NodeServices;
 use lightning_signer::persist::Persist;
 use lightning_signer::signer::ClockStartingTimeFactory;
 use lightning_signer::util::clock::StandardClock;
-use lightning_signer::util::crypto_utils::maybe_generate_seed;
 use lightning_signer::util::status::Status;
 use lightning_signer_server::persist::kv_json::KVJsonPersister;
 use log::{error, info};
@@ -56,8 +54,7 @@ async fn connect(datadir: &str, uri: Uri, network: Network) {
     info!("ping result {}", reply.message);
     let (sender, receiver) = mpsc::channel(1);
     let response_stream = ReceiverStream::new(receiver);
-    let test_seed = read_integration_test_seed();
-    let seed = maybe_generate_seed(test_seed);
+    let seed = integration_test_seed_or_generate();
     let persister: Arc<dyn Persist> = Arc::new(KVJsonPersister::new(&data_path));
     let allowlist = read_allowlist();
     let starting_time_factory = ClockStartingTimeFactory::new();
@@ -66,10 +63,6 @@ async fn connect(datadir: &str, uri: Uri, network: Network) {
     let services = NodeServices { validator_factory, starting_time_factory, persister, clock };
     let handler_builder =
         RootHandlerBuilder::new(network, 0, services, seed).allowlist(allowlist.clone());
-    // if no seed was provided by the integration test framework, persist the seed that we generated
-    if test_seed.is_none() {
-        write_integration_test_seed(&seed);
-    }
     let (root_handler, _muts) = handler_builder.build();
 
     // NOTE - For this signer mode it is easier to use the ALLOWLIST file to maintain the

vls-proxy/src/util.rs
@@ -9,6 +9,7 @@ use std::{env, fs};
 use lightning_signer::bitcoin::Network;
 use lightning_signer::policy::filter::PolicyFilter;
 use lightning_signer::policy::simple_validator::{make_simple_policy, SimpleValidatorFactory};
+use lightning_signer::util::crypto_utils::generate_seed;
 use lightning_signer::Arc;
 use tokio::runtime::{self, Runtime};
 
@@ -32,8 +33,20 @@ pub fn read_integration_test_seed() -> Option<[u8; 32]> {
     }
 }
 
-pub fn write_integration_test_seed(seed: &[u8; 32]) {
+fn write_integration_test_seed(seed: &[u8; 32]) {
     fs::write("hsm_secret", seed).expect("trouble writing hsm_secret");
+}
+
+/// Read integration test seed, and generate/persist it if it's missing
+pub fn integration_test_seed_or_generate() -> [u8; 32] {
+    match read_integration_test_seed() {
+        None => {
+            let seed = generate_seed();
+            write_integration_test_seed(&seed);
+            seed
+        }
+        Some(seed) => seed,
+    }
 }
 
 #[cfg(feature = "main")]

vls-proxy/src/vls_main.rs
@@ -18,7 +18,7 @@ use lightning_signer::node::NodeServices;
 use lightning_signer::persist::{Mutations, Persist};
 use lightning_signer::signer::ClockStartingTimeFactory;
 use lightning_signer::util::clock::StandardClock;
-use lightning_signer::util::crypto_utils::{hkdf_sha256, maybe_generate_seed};
+use lightning_signer::util::crypto_utils::hkdf_sha256;
 use lightning_signer::Arc;
 use lightning_signer_server::nodefront::SingleFront;
 use lightning_signer_server::persist::kv_json::KVJsonPersister;
@@ -37,8 +37,8 @@ use vls_protocol_signer::vls_protocol;
 
 mod test;
 use vls_proxy::util::{
-    add_hsmd_args, bitcoind_rpc_url, handle_hsmd_version, make_validator_factory,
-    read_integration_test_seed, setup_logging, vls_network, write_integration_test_seed,
+    add_hsmd_args, bitcoind_rpc_url, handle_hsmd_version, integration_test_seed_or_generate,
+    make_validator_factory, setup_logging, vls_network,
 };
 use vls_proxy::*;
 
@@ -266,17 +266,11 @@ async fn start() {
     let starting_time_factory = ClockStartingTimeFactory::new();
     let validator_factory = make_validator_factory(network);
     let clock = Arc::new(StandardClock());
-    let test_seed = read_integration_test_seed();
-    let seed = maybe_generate_seed(test_seed);
+    let seed = integration_test_seed_or_generate();
     let persister = make_persister();
     let services = NodeServices { validator_factory, starting_time_factory, persister, clock };
     let handler_builder =
         RootHandlerBuilder::new(network, client.id(), services, seed).allowlist(allowlist);
-
-    // if no seed was provided by the integration test framework, persist the one we generated
-    if test_seed.is_none() {
-        write_integration_test_seed(&seed);
-    }
 
     let looper = make_looper(&seed).await;
 