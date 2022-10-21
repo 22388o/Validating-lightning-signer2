lightning-signer/validating-lightning-signer

vlsd: persist seeds in datadir via FileSeedPersister

lightning-signer-core/src/persist/mod.rs
@@ -201,3 +201,49 @@ impl SeedPersist for MemorySeedPersister {
         Some(self.seed.clone())
     }
 }
+
+#[cfg(feature = "std")]
+/// File system persisters
+pub mod fs {
+    use crate::persist::SeedPersist;
+    use bitcoin::hashes::hex::{FromHex, ToHex};
+    use bitcoin::secp256k1::PublicKey;
+    use std::fs;
+    use std::path::PathBuf;
+
+    /// A file system directory seed persister
+    ///
+    /// Stores seed in a file named `<node_id_hex>.seed` in the directory
+    pub struct FileSeedPersister {
+        path: PathBuf,
+    }
+
+    impl FileSeedPersister {
+        /// Create
+        pub fn new<P: Into<PathBuf>>(path: P) -> Self {
+            Self { path: path.into() }
+        }
+    }
+
+    impl SeedPersist for FileSeedPersister {
+        fn put(&self, node_id: &PublicKey, seed: &[u8]) {
+            let mut path = self.path.clone();
+            path.push(format!("{}.seed", node_id));
+            write_seed(path, seed);
+        }
+
+        fn get(&self, node_id: &PublicKey) -> Option<Vec<u8>> {
+            let mut path = self.path.clone();
+            path.push(format!("{}.seed", node_id));
+            read_seed(path)
+        }
+    }
+
+    fn write_seed(path: PathBuf, seed: &[u8]) {
+        fs::write(path, seed.to_hex()).unwrap();
+    }
+
+    fn read_seed(path: PathBuf) -> Option<Vec<u8>> {
+        fs::read_to_string(path).ok().map(|s| Vec::from_hex(&s).expect("bad hex seed"))
+    }
+}

22388o/Validating-lightning-signer2

lightning-signer-core/src/persist/mod.rs
@@ -162,11 +162,16 @@ impl Persist for DummyPersister {
 }
 
 /// Seed persister
+///
+/// By convention, the `key` is the hex-encoded public key, but this may change
+/// in the future.
 pub trait SeedPersist: Sync + Send {
     /// Persist the seed
     fn put(&self, key: &str, seed: &[u8]);
     /// Get the seed, if exists
     fn get(&self, key: &str) -> Option<Vec<u8>>;
+    /// List the seeds
+    fn list(&self) -> Vec<String>;
 }
 
 /// A null seed persister for testing
@@ -177,6 +182,10 @@ impl SeedPersist for DummySeedPersister {
 
     fn get(&self, _key: &str) -> Option<Vec<u8>> {
         None
+    }
+
+    fn list(&self) -> Vec<String> {
+        Vec::new()
     }
 }
 
@@ -199,5 +208,69 @@ impl SeedPersist for MemorySeedPersister {
 
     fn get(&self, _key: &str) -> Option<Vec<u8>> {
         Some(self.seed.clone())
+    }
+
+    fn list(&self) -> Vec<String> {
+        Vec::new()
+    }
+}
+
+#[cfg(feature = "std")]
+/// File system persisters
+pub mod fs {
+    use crate::persist::SeedPersist;
+    use bitcoin::hashes::hex::{FromHex, ToHex};
+    use std::fs;
+    use std::path::PathBuf;
+
+    /// A file system directory seed persister
+    ///
+    /// Stores seed in a file named `<node_id_hex>.seed` in the directory
+    pub struct FileSeedPersister {
+        path: PathBuf,
+    }
+
+    impl FileSeedPersister {
+        /// Create
+        pub fn new<P: Into<PathBuf>>(path: P) -> Self {
+            Self { path: path.into() }
+        }
+
+        fn seed_path_for_key(&self, node_id: &str) -> PathBuf {
+            let mut path = self.path.clone();
+            path.push(format!("{}.seed", node_id));
+            path
+        }
+    }
+
+    impl SeedPersist for FileSeedPersister {
+        fn put(&self, key: &str, seed: &[u8]) {
+            write_seed(self.seed_path_for_key(key), seed);
+        }
+
+        fn get(&self, key: &str) -> Option<Vec<u8>> {
+            read_seed(self.seed_path_for_key(key))
+        }
+
+        fn list(&self) -> Vec<String> {
+            let mut keys = Vec::new();
+            for entry in fs::read_dir(&self.path).unwrap() {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if path.extension().unwrap() == "seed" {
+                    let key = path.file_stem().unwrap().to_str().unwrap();
+                    keys.push(key.to_string());
+                }
+            }
+            keys
+        }
+    }
+
+    fn write_seed(path: PathBuf, seed: &[u8]) {
+        fs::write(path, seed.to_hex()).unwrap();
+    }
+
+    fn read_seed(path: PathBuf) -> Option<Vec<u8>> {
+        fs::read_to_string(path).ok().map(|s| Vec::from_hex(&s).expect("bad hex seed"))
     }
 }